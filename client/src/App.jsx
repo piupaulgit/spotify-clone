8895d56392c8fa4166656df43a86d3579fecc650
piupaulgit <contactpiu619@gmail.com>
2023-01-14 10:09:27 +0530

    auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,27 +4,24 @@ import { useDispatch } from 'react-redux';
 import Spotify from './components/Spotify';
 import Login from './pages/Login';
 import { setUserToken } from './redux/features/authSlice';
+const code = new URLSearchParams(window.location.search).get("code")
 
 const App = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
+  // console.log(code)
+  // const dispatch = useDispatch();
+  // const [token,setToken] = useState("")
+  // useEffect(() => {
    
-    const hash = window.location.hash;
-    if(hash){
-      const tkn = hash.substring(1).split("&")[0].split("=")[1]
-      localStorage.setItem("token",tkn)
-       dispatch(setUserToken(tkn))
-       window.history.pushState({},null,'/')
-    } 
+  //   const hash = window.location.hash;
+  //   if(hash){
+  //     const tkn = hash.substring(1).split("&")[0].split("=")[1]
+  //     localStorage.setItem("token",tkn)
+  //      dispatch(setUserToken(tkn))
+  //     //  window.history.pushState({},null,'/')
+  //   } 
     
-  },[])
-  return (
-    <>
-      {
-        !token ? <Login/>:<Spotify/>
-      }
-    </>
-  )
+  // },[token])
+  return code ? <Spotify code={code} /> : <Login />
 };
 
 export default App;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,23 +2,22 @@ import React from "react";
 import { blackLogo } from "../assets";
 
 const Login = () => {
-    const handleClick = async () => {
-        const client_id = "2dba1902f6c941aeb5d5f283a7e2e873";
-        const redirect_uri = "http://localhost:3000/";
-        const api_uri = "https://accounts.spotify.com/authorize";
-        const scope = [
-          "user-read-private",
-          "user-read-email",
-          "user-modify-playback-state",
-          "user-read-playback-state",
-          "user-read-currently-playing",
-          "user-read-recently-played",
-          "user-top-read",
-        ];
-        window.location.href = `${api_uri}?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope.join(
-          " "
-        )}&response_type=token&show_dialog=true`;
-      };
+  const client_id = "2dba1902f6c941aeb5d5f283a7e2e873";
+  const redirect_uri = "http://localhost:3000/";
+  const api_uri = "https://accounts.spotify.com/authorize";
+  const scope = [
+    "user-read-private",
+    "user-read-email",
+    "user-modify-playback-state",
+    "user-read-playback-state",
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-top-read",
+  ];
+
+  const AUTH_URL = `${api_uri}?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope.join(
+    " "
+  )}&response_type=code`
 
   return (
     <div className="h-screen bg-[#1CDF63] flex justify-center items-center">
@@ -29,9 +28,9 @@ const Login = () => {
           width={400}
           className=" mb-10"
         />
-        <button onClick={handleClick} className="transition text-xl bg-black rounded-full px-20 py-4 text-[#1CDF63] hover:bg-[#ffffff]">
+        <a href={AUTH_URL} className="transition text-xl bg-black rounded-full px-20 py-4 text-[#1CDF63] hover:bg-[#ffffff]">
           Connect Spotify
-        </button>
+        </a>
       </div>
     </div>
   );
