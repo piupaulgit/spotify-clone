5e52e43c85ff16db1195897d782272c0512c08fa
piupaulgit <contactpiu619@gmail.com>
2023-01-15 23:25:55 +0530

    server

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors")
+const spotifyWebApi = require("spotify-web-api-node");
+const bodyParser = require("body-parser");
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+app.post("/login", (req,res) => {
+    const code = req.body.code;
+    const credentials = new spotifyWebApi({
+        redirectUri:"http://localhost:3000/",
+        clientId:"2dba1902f6c941aeb5d5f283a7e2e873",
+        clientSecret:"347c91077923427abd5f4444ffce0246"
+    })
+    const spotifyApi = new SpotifyWebApi(credentials);
+
+    spotifyApi.authorizationCodeGrant(code).then(
+        function(data) {
+          console.log('The token expires in ' + data.body['expires_in']);
+          console.log('The access token is ' + data.body['access_token']);
+          console.log('The refresh token is ' + data.body['refresh_token']);
+      
+          // Set the access token on the API object to use it in later calls
+          spotifyApi.setAccessToken(data.body['access_token']);
+          spotifyApi.setRefreshToken(data.body['refresh_token']);
+        },
+        function(err) {
+          console.log('Something went wrong!', err);
+        }
+      );
+})
+
