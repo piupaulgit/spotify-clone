15858cc296a8abcfbe227f717671274c368b039a
Piu Paul <31591817+piupaulgit@users.noreply.github.com>
2023-06-21 22:32:48 +0530

    Update TopBar.jsx

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -8,7 +8,7 @@ const TopBar = () => {
         <BsArrowRightCircle size={24} className="opacity-20 hover:opacity-100 cursor-pointer"/>
       </div>
       <div className="flex">
-        <button className="transition ease-in-out rounded-full px-7 py-2 text-sm border-2 border-[#23D3ED] hover:bg-[#23D3ED]">Upgrade</button>
+{/*         <button className="transition ease-in-out rounded-full px-7 py-2 text-sm border-2 border-[#23D3ED] hover:bg-[#23D3ED]">Upgrade</button> */}
         <button className="rounded-full ease-in-out ml-4 px-7 py-2 text-sm bg-[#23D3ED] hover:bg-[#000]">Piu Paul</button>
       </div>
     </div>
